c86aef24e291dae0cb723934a31f2d1e01900815
jfqd <jfqd@blun.org>
2022-04-06 09:46:20 +0200

    use custom conf file if present

--- copy/var/zoneinit/includes/41-gogs.sh
+++ copy/var/zoneinit/includes/41-gogs.sh
@@ -10,5 +10,15 @@ gsed -i \
 
 svcadm restart svc:/network/ssh:default
 
+if mdata-get vfstab 1>/dev/null 2>&1; then
+  MOUNT_FOLDER=$(mdata-get vfstab | awk '{print $3}')
+  if [[ -f "${MOUNT_FOLDER}/app.ini" ]]; then
+    mkdir -p home/gogs/go/src/github.com/gogs/gogs/custom/conf
+    cp "${MOUNT_FOLDER}/app.ini" /home/gogs/go/src/github.com/gogs/gogs/custom/conf/app.ini
+  else
+    # TODO: setup app.ini via mdata if custom conf is missing
+  fi
+fi
+
 svccfg import /opt/local/lib/svc/manifest/gogs.xml
 svcadm enable -r svc:/application/gogs:default
